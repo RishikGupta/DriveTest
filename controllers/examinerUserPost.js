2e5ac3254b8eee3f0f5c734ba09b23a13070b942
Rishik Gupta <rishik.gupta70@gmail.com>
2024-08-12 19:28:37 -0400

    final_commit

--- controllers/examinerUserPost.js
+++ controllers/examinerUserPost.js
@@ -2,7 +2,7 @@ const User = require('../models/User');
 module.exports = async (req, res) => {
     try {
         let id = req.body.mongo_id;
-        let pass_fail = (req.body.pass_fail == "true")? true : false;
+        let pass_fail = (req.body.pass_fail == "true") ? true : false;
         let comment = req.body.comment;
         await User.findByIdAndUpdate(id, {
             pass_fail: pass_fail,
@@ -12,5 +12,5 @@ module.exports = async (req, res) => {
     } catch (error) {
         console.log("Error in updating user in db in ExaminerUserPost" + error);
     }
-    
+
 }
